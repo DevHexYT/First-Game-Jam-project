315f9265a74d642483105d8440933b1bbb202171
DevHexYT <yairbak@gmail.com>
2025-02-11 20:21:09 +0200

    Revert "Some Prefabs added and some script improvements"
    This reverts commit 475b8a035ade19d3c01808d21fc2d2a43101d36a.

--- Assets/Prefab/GameMannge/GameManngeScript.cs
+++ Assets/Prefab/GameMannge/GameManngeScript.cs
@@ -5,13 +5,8 @@ public class GameManngeScript : MonoBehaviour {
 	public static GameManngeScript instance;
 	public int enemysCount;
 
-	private GameObject staminaCanvas;
-	private GameObject winCanvas;
-
-	private void Awake() {
-		winCanvas = transform.Find("Win").gameObject;
-		staminaCanvas = transform.Find("Health_Stamina Bars").gameObject;
-	}
+	[SerializeField] private GameObject youWinCanvas;
+	[SerializeField] private GameObject staminaCanvas;
 
 	private void Update() {
 		CheckEnemys();
@@ -24,7 +19,7 @@ public class GameManngeScript : MonoBehaviour {
 	}
 	public void CheckEnemys() {
 		if (enemysCount == 0) {
-			winCanvas.SetActive(true);
+			youWinCanvas.SetActive(true);
 			staminaCanvas.SetActive(false);
 			PlayerScript plrScript = GameObject.Find("Player").GetComponent<PlayerScript>();
 			plrScript.winLevel = true;
